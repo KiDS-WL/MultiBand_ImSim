0b9fc84977b79a0fa32ac7c854ccf09d06b0bf9e
lshuns <1006682997@qq.com>
2021-11-09 14:49:47 +0100

    example for skills r-band

--- scripts/example_run_skills.sh
+++ scripts/example_run_skills.sh
@@ -1,7 +1,7 @@
 # @Author: lshuns
 # @Date:   2021-06-25 18:42:24
 # @Last Modified by:   lshuns
-# @Last Modified time: 2021-08-27 14:38:53
+# @Last Modified time: 2021-11-09 13:17:43
 
 # 0: generate an example configuration file\n\
 # 1: simulate images\n\
@@ -12,9 +12,6 @@
 # 6: measure shapes\n\
 # 7: create a combined catalogue\n\
 
-# ## All possible g1,g2 combinations
-# g1Range = np.array([-0.0283,+0.0283,+0.0283,-0.0283])
-# g2Range = np.array([+0.0283,+0.0283,-0.0283,-0.0283])
+python ../modules/Run.py 1 2 3 --runTag p283p283 --threads 48 --rng_seed 94012010 --cosmic_shear 0.0283 0.0283 -c ./example_skills_bandr.ini --sep_running_log
 
-python ../modules/Run.py 1 2 3 7 --runTag m283p283 --threads 4 --rng_seed 940120 --cosmic_shear -0.0283 0.0283 -c ./example_skills_bandr.ini --sep_running_log
-
+python ../modules/Run.py 1 2 3 --runTag m283m283 --threads 48 --rng_seed 94012030 --cosmic_shear -0.0283 -0.0283 -c ./example_skills_bandr.ini --sep_running_log
