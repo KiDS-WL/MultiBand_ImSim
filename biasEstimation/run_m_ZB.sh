251215c009f4e547c8beae26b332172943ec22d9
lshuns <1006682997@qq.com>
2023-04-11 14:10:58 +0200

    update biasEstimation with full K1000-LF321 cata

--- biasEstimation/run_m_ZB.sh
+++ biasEstimation/run_m_ZB.sh
@@ -1,7 +1,7 @@
 # @Author: lshuns
-# @Date:   2022-09-19 16:34:10
+# @Date:   2023-02-04 16:04:20
 # @Last Modified by:   lshuns
-# @Last Modified time: 2022-09-19 17:43:19
+# @Last Modified time: 2023-04-11 14:08:00
 
 # usage: bias_estimate_script.py [-h] [--bias_type {alpha_LS,m_LS_pair,m_LS_tile}] [--in_file IN_FILE]
 #                                [--out_path OUT_PATH] [--cols_e12 COLS_E12 COLS_E12]
@@ -13,43 +13,23 @@
 #                                [--binning_min_max_Nbin BINNING_MIN_MAX_NBIN BINNING_MIN_MAX_NBIN BINNING_MIN_MAX_NBIN]
 #                                [--binning_type {linear,log,quantile}] [--psf_frame]
 
-
-# ###### v07D7: constShear whole (kids photometry)
-# python bias_estimate_script.py --bias_type m_LS_tile\
-#                                 --in_file /disks/shear16/ssli/ImSim/output/skills_v07D7/skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.feather\
-#                                 --out_path ./outputs/m_weiRaw_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.csv\
-#                                 --cols_e12 e1_LF_r e2_LF_r\
-#                                 --e_type measured\
-#                                 --cols_g12 g1_in g2_in\
-#                                 --col_weight oldweight_LF_r\
-#                                 --col_label tile_label\
-#                                 --col_binning Z_B\
-#                                 --binning_edges 0.1 0.3 0.5 0.7 0.9 1.2 2.0
-# binning on: Z_B
-# provided bins [0.1 0.3 0.5 0.7 0.9 1.2 2. ]
-# using tile based mCalFunc
-# Number of sources in the catalogue 47870631
-# selected objects (within bin edge) 47499698
-# selected objects (weight>0) 38907653
-# results saved to ./outputs/m_weiRaw_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.csv
-# Elapsed:6:07.73,User=265.892,System=1526.923,CPU=487.5%.
-
-###### v07D7: constShear whole (kids photometry), After AlphaRecal
+# ###### v07D7p1: constShear whole (kids photometry)
 python bias_estimate_script.py --bias_type m_LS_tile\
-                                --in_file /disks/shear16/ssli/ImSim/output/skills_v07D7/skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_A1_A2.feather\
-                                --out_path ./outputs/m_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_A12.csv\
-                                --cols_e12 AlphaRecalD2_e1 AlphaRecalD2_e2\
+                                --in_file /disks/shear16/ssli/ImSim/output/skills_v07D7/skills_v07D7p1_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.feather\
+                                --out_path ./results/m_weiRaw_skills_v07D7p1_kidsPhotometry.csv\
+                                --cols_e12 e1_LF_r e2_LF_r\
                                 --e_type measured\
                                 --cols_g12 g1_in g2_in\
-                                --col_weight AlphaRecalC_weight\
+                                --col_weight oldweight_LF_r\
                                 --col_label tile_label\
                                 --col_binning Z_B\
                                 --binning_edges 0.1 0.3 0.5 0.7 0.9 1.2 2.0
+# # ### out info
 # binning on: Z_B
 # provided bins [0.1 0.3 0.5 0.7 0.9 1.2 2. ]
 # using tile based mCalFunc
-# Number of sources in the catalogue 38759673
-# selected objects (within bin edge) 38759673
-# selected objects (weight>0) 38759673
-# results saved to ./outputs/m_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_A12.csv
-# Elapsed:5:32.71,User=232.120,System=1327.859,CPU=468.8%.
+# Number of sources in the catalogue 47869587
+# selected objects (within bin edge) 47510465
+# selected objects (weight>0) 38916184
+# results saved to ./results/m_weiRaw_skills_v07D7p1_kidsPhotometry.csv
+# Elapsed:5:41.48,User=270.147,System=1405.989,CPU=490.8%.
--- biasEstimation/run_m_ZB_dataRewei.sh
+++ biasEstimation/run_m_ZB_dataRewei.sh
@@ -1,7 +1,7 @@
 # @Author: lshuns
-# @Date:   2022-08-17 15:04:38
+# @Date:   2023-02-04 16:04:35
 # @Last Modified by:   lshuns
-# @Last Modified time: 2022-09-29 13:00:25
+# @Last Modified time: 2023-04-11 13:53:47
 
 # usage: bias_estimate_script_dataRewei.py [-h]
 #                                          [--bias_type {m_LS_DataRewei_2D}]
@@ -24,54 +24,54 @@
 #                                          [--save_surface_prefix SAVE_SURFACE_PREFIX]
 #                                          [--save_bounds_prefix SAVE_BOUNDS_PREFIX]
 
-###### v07D7: constShear whole (kids photometry)
+###### v07D7p1: constShear whole (kids photometry)
 python bias_estimate_script_dataRewei.py --bias_type m_LS_DataRewei_2D\
-                                --in_file /disks/shear16/ssli/ImSim/output/skills_v07D7/skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.feather\
-                                --out_path ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.csv\
+                                --in_file /disks/shear16/ssli/ImSim/output/skills_v07D7/skills_v07D7p1_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.feather\
+                                --out_path ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry.csv\
                                 --cols_e12 e1_LF_r e2_LF_r\
                                 --cols_g12 g1_in g2_in\
                                 --col_weight_sim_data oldweight_LF_r weight\
                                 --col_label tile_label\
                                 --col_binning_sim_data Z_B Z_B\
                                 --binning_edges 0.1 0.3 0.5 0.7 0.9 1.2 2.0\
-                                --in_file_data /disks/shear16/ssli/KiDS/K1000_forSKiLLS/kids_photo_LF_321_noSG_noWeiCut_newCut_817tiles.feather\
+                                --in_file_data /disks/shear10/ssli/KiDS/K1000_LF_321_mosaic/KIDS_1006tiles_r_SDSS.V1.0.0A_ugriZYJHKs_photoz_SG_mask_LF_glab_321_v2_selec_noWeiCut.feather\
                                 --bin1_info SNR_LF_r model_SNratio 20\
                                 --bin2_info R R 20\
                                 --fitting_method tile_based\
-                                --save_surface_prefix ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut\
-                                --save_bounds_prefix ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut
-# ### out info
+                                --save_surface_prefix ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry\
+                                --save_bounds_prefix ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry
+
+# # ### out info
 # binning on: ['Z_B', 'Z_B']
 # provided bins [0.1 0.3 0.5 0.7 0.9 1.2 2. ]
 # using data reweighting in 2 parameters
 #      with the tile based mCalFunc
-# Number of sources in the data 31497513
-#      within the edges 31200045
-#      with weight > 0 26453911
-# Number of sources in the simulation 47870631
-#      within the edges 47499698
-#      with weight > 0 38907653
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_whole_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_whole_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_whole.csv
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin0_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin0_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin0.csv
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin1_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin1_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin1.csv
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin2_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin2_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin2.csv
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin3_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin3_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin3.csv
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin4_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin4_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin4.csv
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin5_bin1.npy
-# bin1_bounds saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin5_bin2.npy
-# the bias surface saved as ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut_bin5.csv
-# results saved to ./outputs/m_weiRaw_DR4817Rewei_skills_v07D7_LF_321_kidsPhotometry_shear_noSG_noWeiCut_newCut.csv
-# Elapsed:12:38.16,User=593.538,System=1898.794,CPU=328.7%.
-
+# Number of sources in the data 38026162
+#      within the edges 37667985
+#      with weight > 0 31869107
+# Number of sources in the simulation 47869587
+#      within the edges 47510465
+#      with weight > 0 38916184
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_whole_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_whole_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_whole.csv
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin0_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin0_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin0.csv
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin1_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin1_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin1.csv
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin2_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin2_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin2.csv
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin3_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin3_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin3.csv
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin4_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin4_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin4.csv
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin5_bin1.npy
+# bin1_bounds saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin5_bin2.npy
+# the bias surface saved as ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry_bin5.csv
+# results saved to ./results/m_weiRaw_K1000_LF_321_skills_v07D7p1_kidsPhotometry.csv
+# Elapsed:16:33.36,User=646.704,System=2320.450,CPU=298.6%.
